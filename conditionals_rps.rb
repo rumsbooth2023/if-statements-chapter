d5afc4269f78c3c978d9c0a9f4581aa997f3705d
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-06 01:18:14 +0000

    RPS Complete

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,18 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+player_input = gets.chomp
+
+p "You played " + player_input.to_s + "!"
+p "The computer played scissors!"
+if player_input == "scissors"
+  p "You tied!"
+end
+
+if player_input == "rock"
+  p "You won!"
+end
+
+if player_input == "paper"
+  p "You lost!"
+end
